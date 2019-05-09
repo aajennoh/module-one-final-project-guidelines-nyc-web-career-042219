303f3132199300e9b7226768653aa2c8483591a5
github username <github email address>
2019-05-09 16:31:01 -0400

    woohoo

--- app/command_line_interface.rb
+++ app/command_line_interface.rb
@@ -3,6 +3,7 @@ require_relative '../app/Compliment.rb'
 require_relative '../app/UserCompliment.rb'
 require_relative '../app/UserContact.rb'
 require_relative '../app/texting.rb'
+require 'colorize'
 ActiveRecord::Base.logger = nil
 
 class CommandLineInterface
@@ -10,28 +11,28 @@ class CommandLineInterface
   attr_accessor :user, :contact_user, :command, :compliment, :contact_check
 
   def logo_message
-    puts "==================================================================="
+    puts "===================================================================================="
      puts "
-  ,--.   ,--,--.
-  |  |   |  |  ,---.,--. ,--.
-  |  |.'.|  |  .-.  |\  '  /
-  |   ,'.   |  | |  | \   ,--.
-  '--'   '--`--' `--.-'  /'-,/                                    ,---.
-  ,--------,--.     `---'        ,--.        ,--.   ,--.          |   |
-  '--.  .--|  ,---. ,--,--,--,--,|  |,-.      \  `.'  ,---.,--.,--|  .'
-     |  |  |  .-.  ' ,-.  |      |     /       '.    | .-. |  ||  |  |
-     |  |  |  | |  \ '-'  |  ||  |  \  \         |  |' '-' '  ''  `--'
-     `--'  `--' `--'`--`--`--''--`--'`--'        `--' `---' `----'.--.
-                                                                  '--'  "
+     ██╗    ██╗██╗  ██╗██╗   ██╗
+     ██║    ██║██║  ██║╚██╗ ██╔╝
+     ██║ █╗ ██║███████║ ╚████╔╝
+     ██║███╗██║██╔══██║  ╚██╔╝
+     ╚███╔███╔╝██║  ██║   ██║▄█╗
+      ╚══╝╚══╝ ╚═╝  ╚═╝   ╚═╝╚═╝
 
+     ████████╗██╗  ██╗ █████╗ ███╗   ██╗██╗  ██╗    ██╗   ██╗ ██████╗ ██╗   ██╗██╗
+     ╚══██╔══╝██║  ██║██╔══██╗████╗  ██║██║ ██╔╝    ╚██╗ ██╔╝██╔═══██╗██║   ██║██║
+        ██║   ███████║███████║██╔██╗ ██║█████╔╝      ╚████╔╝ ██║   ██║██║   ██║██║
+        ██║   ██╔══██║██╔══██║██║╚██╗██║██╔═██╗       ╚██╔╝  ██║   ██║██║   ██║╚═╝
+        ██║   ██║  ██║██║  ██║██║ ╚████║██║  ██╗       ██║   ╚██████╔╝╚██████╔╝██╗
+        ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝       ╚═╝    ╚═════╝  ╚═════╝ ╚═╝ ".colorize(:light_blue).blink
 
-  puts "====================================================================="
-
+  puts "===================================================================================="
   end
 
   def welcome
-    puts "Welcome to Why, Thank You!"
-    puts "Give personalized compliments to your friends or yourself!"
+    puts "Welcome to Why, Thank You!".colorize(:light_blue).bold
+    puts "Give personalized compliments to your friends or yourself!".colorize(:light_blue).bold
     puts "======================================"
     welcome_or_create
   end
@@ -42,10 +43,12 @@ class CommandLineInterface
 
     if User.find_by(user_data) == nil
       @user = User.create(user_data)
-      puts "Pleasure to meet you #{@user.first_name}!"
+      puts "Pleasure to meet you #{@user.first_name}!".green.bold
+      puts "------------------------"
     else
       @user = User.find_by(user_data)
-      puts "Great having you back #{@user.first_name}!"
+      puts "Great having you back #{@user.first_name}!".green.bold
+      puts "------------------------"
     end
     display_menu
   end
@@ -66,13 +69,13 @@ class CommandLineInterface
   end
 
   def display_menu
-    puts "MAIN MENU"
+    puts "MAIN MENU".colorize(:light_blue).bold
     puts "======================================"
-    puts "Enter 's' to send a compliment"
-    puts "Enter 'r' to receive a compliment"
-    puts "Enter 'c' to view your contacts"
-    puts "Enter 'v' to view all the compliments you have generated"
-    puts "Enter 'q' to quit"
+    puts "Enter 's' to send a compliment".colorize(:yellow)
+    puts "Enter 'r' to receive a compliment".colorize(:yellow)
+    puts "Enter 'c' to view your contacts".colorize(:yellow)
+    puts "Enter 'v' to view all the compliments you have generated".colorize(:yellow)
+    puts "Enter 'q' to quit".colorize(:yellow)
     puts "======================================"
 
     get_menu_command
@@ -95,16 +98,16 @@ class CommandLineInterface
     elsif @command == 'q'
       quit()
     else
-      puts "Please enter a valid command"
+      puts "Please enter a valid command".colorize(:red)
       get_menu_command()
     end
   end
 
   def get_compliment_command
     puts "======================================"
-    puts "Enter '1' for a badass compliment"
-    puts "Enter '2' for a quirky compliment"
-    puts "Enter '3' to go back."
+    puts "Enter '1' for a badass compliment".colorize(:yellow)
+    puts "Enter '2' for a quirky compliment".colorize(:yellow)
+    puts "Enter '3' to go back to main menu".colorize(:yellow)
     puts "======================================"
 
     @compliment_command = gets.chomp
@@ -160,9 +163,11 @@ class CommandLineInterface
       else
         process_compliment_command()
       end
+    elsif @compliment_command == '3'
+      display_menu()
 
     else
-      puts "Please enter a valid command"
+      puts "Please enter a valid command".colorize(:red)
       get_compliment_command()
     end
   end
@@ -170,7 +175,8 @@ class CommandLineInterface
   def contact_creation(user_data)
     @contact_user = User.find_or_create_by(user_data)
     UserContact.create(user_id: @user.id, contact_id: @contact_user.id)
-    puts "#{@contact_user.first_name} has been saved in your contacts!"
+    puts "#{@contact_user.first_name} has been saved in your contacts!".colorize(:green).bold
+    sleep 1.2
   end
 
   def display_contacts
@@ -184,8 +190,9 @@ class CommandLineInterface
   def get_contact_command
     display_contacts
     puts "======================================"
-    puts "Enter contact name to send them a compliment"
-    puts "Enter 'b' to go back to main menu"
+    puts "Enter contact name to send them a compliment".colorize(:yellow)
+    puts "Enter 'a' to add a contact".colorize(:yellow)
+    puts "Enter 'b' to go back to main menu".colorize(:yellow)
     puts "======================================"
     @command = gets.chomp
     @contact_check = @command.split(" ")
@@ -199,14 +206,19 @@ class CommandLineInterface
   def process_contact_command
     if @command == 'b'
       display_menu()
+    elsif @command == 'a'
+      user_data = data_collection()
+      contact_creation(user_data)
+      get_contact_command()
     elsif check_contacts()
         @command = 's'
         @contact_user = check_contacts()
         get_compliment_command()
     else
       puts "======================================"
-      puts "That person is not in your contacts!"
+      puts "That person is not in your contacts!".colorize(:red)
       puts "======================================"
+      sleep 1.2
       get_contact_command()
     end
   end
@@ -223,8 +235,8 @@ class CommandLineInterface
 
   def get_send_command
     puts "======================================"
-    puts "Enter 'a' to send to one of your contacts"
-    puts "Enter 'b' to send to a new contact"
+    puts "Enter 'a' to send to one of your contacts".colorize(:yellow)
+    puts "Enter 'b' to send to a new contact".colorize(:yellow)
     puts "======================================"
 
     @command = gets.chomp
@@ -259,15 +271,15 @@ class CommandLineInterface
   def display_compliment
     puts "Here is the compliment you generated"
     puts "======================================"
-    puts @compliment.content
+    puts @compliment.content.green.bold
     puts "======================================"
   end
 
   def get_refresh_command
     display_compliment
     puts "======================================"
-    puts "Enter '1' to send this compliment"
-    puts "Enter '2' to generate a new compliment"
+    puts "Enter '1' to send this compliment".yellow
+    puts "Enter '2' to generate a new compliment".yellow
     puts "======================================"
     @refresh_command = gets.chomp
     process_refresh_command
@@ -279,21 +291,23 @@ class CommandLineInterface
     elsif @refresh_command == '2'
       false
     else
-      puts "Please enter a valid command"
+      puts "Please enter a valid command".colorize(:red)
       get_refresh_command()
     end
   end
 
   def sent_compliment
+    pid = fork{ exec 'afplay', "success.mp3" }
     puts "======================================"
-    puts "Your compliment has been sent! Nice!"
+    puts "Your compliment has been sent! Nice!".colorize(:green).bold
     puts "======================================"
   end
 
   def quit
     puts "======================================"
-    puts "Why, Thank You for using our service, goodbye!"
+    puts "Why, Thank You for using our service, goodbye!".colorize(:light_blue).bold.blink
     puts "======================================"
+    pid = fork{ exec 'afplay', "thanks!.wav" }
     exit(true)
   end
 
